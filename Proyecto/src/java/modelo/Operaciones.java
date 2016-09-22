f536ee2eb44d12d8c08802fabedb73e07d3f0d35
Panta777 <panle777@gmail.com>
2016-09-22 15:38:52 -0600

    Actualizaciones 22/09/2016

--- Proyecto/src/java/modelo/Operaciones.java
+++ Proyecto/src/java/modelo/Operaciones.java
@@ -12,21 +12,20 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 /**
- *
  * @author panle
  */
 public class Operaciones {
 
     /**
-     * Clase para manejar la conectividad a base de datos. Lee variables de
-     * ambiente y recursos del archivo context.xml
+     * Clase para manejar la conectividad a base de datos.
+     * Lee variables de ambiente y recursos del archivo context.xml
      *
      * @author panle
-     *
      */
     private Connection currentConnection = null;
     public Statement stQuery = null;
@@ -38,16 +37,13 @@ public class Operaciones {
     public String DtePath = "";
 
     public Operaciones() {
-        utileria = new Utileria();
-        //variablesAmbiente = new EnvironmentVars();
+        utileria = new Utileria(); //variablesAmbiente = new EnvironmentVars();
     }
 
     /**
-     * Retorna el objeto Connection, que es una nueva conexion a la base de
-     * datos.
+     * Retorna el objeto Connection, que es una nueva conexion a la base de datos.
      *
      * @return currentConnection Connection.
-     *
      */
     public Connection getNewConnection() {
 
@@ -60,15 +56,13 @@ public class Operaciones {
 //        String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
 
         // String USERNAME = "C##PRUEBAS";
-       String USERNAME = "PRUEBAS";
+        String USERNAME = "PRUEBAS";
         String PASSWORD = "medrano7";
         String CLASSNAME = "oracle.jdbc.driver.OracleDriver";
-         String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+        String URL = "jdbc:oracle:thin:@localhost:1521:xe";
         // String URL = "jdbc:oracle:thin:@localhost:1521:orclsys";
 
         try {
-
-
             Class.forName(CLASSNAME);
 //            currentConnection = DriverManager.getConnection(
 //                    (String)variablesAmbiente.getDB_URL(),
@@ -104,20 +98,6 @@ public class Operaciones {
 //                Class.forName("com.mysql.jdbc.Driver").newInstance();
 //                ds = (DataSource) variablesAmbiente.getCurrentContext().lookup("java:comp/env/MYSQLDS");
 //            }
-//
-//            if (variablesAmbiente.getDbDataSourceName().equals("MSSQL")) {   // Microsoft SQLSERVER
-//                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-//                ds = (DataSource) variablesAmbiente.getCurrentContext().lookup("java:comp/env/MSSQLDS");
-//            }
-//            if (variablesAmbiente.getDbDataSourceName().equals("ORACLE")) {  // ORACLE
-//                Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
-//                ds = (DataSource) variablesAmbiente.getCurrentContext().lookup("java:comp/env/ORACLEDS");
-//            }
-//            if (variablesAmbiente.getDbDataSourceName().equals("SQLITE")) {  // ORACLE
-//                Class.forName("org.sqlite.JDBC").newInstance();
-//                ds = (DataSource) variablesAmbiente.getCurrentContext().lookup("java:comp/env/SQLITEDS");
-//            }
-//
 //        } catch (NamingException nE) {
 //            utileria.escribirLog("N/A", "", "\n\nConnectionBD->getDBDataSource()...Error al obtener varaibles del contexto...: " + nE.getMessage());
 //        } catch (Exception cnfE) {
@@ -136,37 +116,26 @@ public class Operaciones {
 //        String usedDataSource = null;
 //
 //        try {
-//
 //            if (variablesAmbiente.getDbDataSourceName().equals("MYSQL")) { // MySQL
 //                usedDataSource = "MYSQL";
-//            }
-//
-//            if (variablesAmbiente.getDbDataSourceName().equals("MSSQL")) { // Microsoft SQLSERVER
-//                usedDataSource = "MSSQL";
-//            }
-//
-//            if (variablesAmbiente.getDbDataSourceName().equals("ORACLE")) { // ORACLE
-//                usedDataSource = "ORACLE";
 //            }
 //        } catch (Exception cnfE) {
 //            utileria.escribirLog("N/A", "", "\n\nConnectionBD->getUsedDBDataSource()...Error, no se pudo instanciar la clase..: " + cnfE.getMessage());
 //        }
-//
 //        return usedDataSource;
 //    }
+
     /**
      * Retorna la coneccion actual de base de datos.
-     *
      * @return the currentConnection
-     *
      */
     public Connection getCurrentConnection() {
         return currentConnection;
     }
 
     /**
-     * Retorna el tipo de Nivel de acceso, según el login de Usuario La cantidad
-     * de "?" determina la cantidad parametros que utiliza el
+     * Retorna el tipo de Nivel de acceso, según el login de Usuario.
+     * La cantidad de "?" determina la cantidad parametros que utiliza el
      * procedimiento/función
      *
      * @param usuario
@@ -179,30 +148,22 @@ public class Operaciones {
         int nivel = 0;
         Connection coneLocal = getNewConnection();
 
-        try
-        {
+        try {
             coneLocal.setAutoCommit(false);
             CallableStatement funcionLogin = coneLocal.prepareCall("{ ?=call GET_TIPO_USUARIO(?,?) }");
-
-            funcionLogin.setString(2, usuario);// cargar parametros de entrada
+            // cargar parametros de entrada
+            funcionLogin.setString(2, usuario);
             funcionLogin.setString(3, contra);
             funcionLogin.registerOutParameter(1, Types.INTEGER);//Parametro de salida
-//            if(funcionLogin.execute()) // ejecutar
-//            {
             funcionLogin.execute();
+
             coneLocal.commit();// confirmar si se ejecuto sin errores
             nivel = funcionLogin.getInt(1);// obtener salida
-//            }
-//            else 
-//                return nivel ; 
-        } catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             coneLocal.rollback();// deshacer la ejecucion en caso de error
             System.out.println("Error al ejecutar función GET_TIPO_USUARIO por, " + e); // informar por consola
-        } 
-        finally 
-        {
-             nivel = 0;
+        } finally {
+            nivel = 0;
             coneLocal.close();// cerrar la conexion
         }
 
@@ -211,11 +172,13 @@ public class Operaciones {
 
     /**
      * Retorna determinado valor, según respuesta de la función para insertar Datos de Nuevo Cliente
+     *
      * @param No_Documento
+     * @param Tipo_Documento
      * @param Nombre
      * @param Apellidos
-     * @param Telefono Residencia
-     * @param Telefono Celular
+     * @param TelefonoResidencia
+     * @param TelefonoCelular
      * @param Nit
      * @param Direccion
      * @param Ciudad
@@ -225,43 +188,187 @@ public class Operaciones {
      * @param Correo
      * @param usuario
      * @param contra
-     * @return      * 1 = exitoso
+     * @return
+     * 1 = exitoso
      * 2 = usuario ya existe
      * 3 =  error al procesar datos
-     * 4 = ?
      * @throws java.sql.SQLException
      */
-    public int insertarCliente(String usuario, String contra) throws SQLException {
-
-        int nivel = 0;
+    public int insertarCliente(String No_Documento, String Tipo_Documento, String Nombre,String Apellidos, String TelefonoResidencia,
+                               String TelefonoCelular, String Nit,String Direccion,String Ciudad,String Departamento, String Pais,
+                                String Profesion, String Correo, String usuario, String contra) throws SQLException {
+        int respuesta = 0;
         Connection coneLocal = getNewConnection();
 
-        try
-        {
+        try {
             coneLocal.setAutoCommit(false);
-            CallableStatement funcionLogin = coneLocal.prepareCall("{ ?=call GET_TIPO_USUARIO(?,?) }");
+            CallableStatement funcionInsertarCliente = coneLocal.prepareCall("{ ?=call xxxxx(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) }");
 
-            funcionLogin.setString(2, usuario);// cargar parametros de entrada
-            funcionLogin.setString(3, contra);
-            funcionLogin.registerOutParameter(1, Types.INTEGER);//Parametro de salida
-//            if(funcionLogin.execute()) // ejecutar
-//            {
-            funcionLogin.execute();
+            // cargar parametros de entrada
+            funcionInsertarCliente.setString(2, No_Documento);
+            funcionInsertarCliente.setString(3, Tipo_Documento);
+            funcionInsertarCliente.setString(4, Nombre);
+            funcionInsertarCliente.setString(5, Apellidos);
+            funcionInsertarCliente.setString(6, TelefonoResidencia);
+            funcionInsertarCliente.setString(7, TelefonoCelular);
+            funcionInsertarCliente.setString(8, Nit);
+            funcionInsertarCliente.setString(9, Direccion);
+            funcionInsertarCliente.setString(10, Ciudad);
+            funcionInsertarCliente.setString(11, Departamento);
+            funcionInsertarCliente.setString(12, Pais);
+            funcionInsertarCliente.setString(13, Profesion);
+            funcionInsertarCliente.setString(14, Correo);
+            funcionInsertarCliente.setString(15, usuario);
+            funcionInsertarCliente.setString(16, contra);
+            funcionInsertarCliente.registerOutParameter(1, Types.INTEGER);//Parametro de salida
+            funcionInsertarCliente.execute();
+
             coneLocal.commit();// confirmar si se ejecuto sin errores
-            nivel = funcionLogin.getInt(1);// obtener salida
-//            }
-//            else
-//                return nivel ;
-        } catch (SQLException e)
+            respuesta = funcionInsertarCliente.getInt(1);// obtener salida
+        }
+        catch (SQLException e)
         {
             coneLocal.rollback();// deshacer la ejecucion en caso de error
-            System.out.println("Error al ejecutar función GET_TIPO_USUARIO por, " + e); // informar por consola
-        }
-        finally
-        {
+            System.out.println("Error al ejecutar función  por, " + e); // informar por consola
+        } finally {
             coneLocal.close();// cerrar la conexion
         }
+        return respuesta;
+    }
 
-        return nivel;
+    /**
+     * Retorna determinado valor, según respuesta de la función para modificar Datos de Cliente
+     *
+     * @param No_Documento
+     * @param Tipo_Documento
+     * @param Nombre
+     * @param Apellidos
+     * @param TelefonoResidencia
+     * @param TelefonoCelular
+     * @param Nit
+     * @param Direccion
+     * @param Ciudad
+     * @param Departamento
+     * @param Pais
+     * @param Profesion
+     * @param Correo
+     * @param usuario
+     * @param contra
+     * @return
+     * 1 = exitoso
+     * 2 = usuario ya existe
+     * 3 =  error al procesar datos
+     * @throws java.sql.SQLException
+     */
+    public int modificarCliente(String No_Documento, String Tipo_Documento, String Nombre,String Apellidos, String TelefonoResidencia,
+                               String TelefonoCelular, String Nit,String Direccion,String Ciudad,String Departamento, String Pais,
+                               String Profesion, String Correo, String usuario, String contra) throws SQLException {
+        int respuesta = 0;
+        Connection coneLocal = getNewConnection();
+
+        try {
+            coneLocal.setAutoCommit(false);
+            CallableStatement funcionmodificarCliente = coneLocal.prepareCall("{ ?=call xxxxx(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) }");
+
+            // cargar parametros de entrada
+            funcionmodificarCliente.setString(2, No_Documento);
+            funcionmodificarCliente.setString(3, Tipo_Documento);
+            funcionmodificarCliente.setString(4, Nombre);
+            funcionmodificarCliente.setString(5, Apellidos);
+            funcionmodificarCliente.setString(6, TelefonoResidencia);
+            funcionmodificarCliente.setString(7, TelefonoCelular);
+            funcionmodificarCliente.setString(8, Nit);
+            funcionmodificarCliente.setString(9, Direccion);
+            funcionmodificarCliente.setString(10, Ciudad);
+            funcionmodificarCliente.setString(11, Departamento);
+            funcionmodificarCliente.setString(12, Pais);
+            funcionmodificarCliente.setString(13, Profesion);
+            funcionmodificarCliente.setString(14, Correo);
+            funcionmodificarCliente.setString(15, usuario);
+            funcionmodificarCliente.setString(16, contra);
+            funcionmodificarCliente.registerOutParameter(1, Types.INTEGER);//Parametro de salida
+            funcionmodificarCliente.execute();
+
+            coneLocal.commit();// confirmar si se ejecuto sin errores
+            respuesta = funcionmodificarCliente.getInt(1);// obtener salida
+        }
+        catch (SQLException e)
+        {
+            coneLocal.rollback();// deshacer la ejecucion en caso de error
+            System.out.println("Error al ejecutar función  por, " + e); // informar por consola
+        } finally {
+            coneLocal.close();// cerrar la conexion
+        }
+        return respuesta;
+    }
+
+    /**
+     * Retorna determinado valor, según respuesta de la función para eliminar a un Cliente por medio de su No. de Documento¿?
+     *
+     * @param IdCliente
+     * @return
+     * 1 = exitoso
+     * 2 = error al procesar datos
+     * @throws java.sql.SQLException
+     */
+    public int eliminarClientePorDocto(String No_Documento) throws SQLException {
+        int respuesta = 0;
+        Connection coneLocal = getNewConnection();
+
+        try {
+            coneLocal.setAutoCommit(false);
+            CallableStatement funcionEliminarCliente = coneLocal.prepareCall("{ ?=call xxxxx(?) }");
+
+            // cargar parametros de entrada
+            funcionEliminarCliente.setString(2, No_Documento);
+            funcionEliminarCliente.registerOutParameter(1, Types.INTEGER);//Parametro de salida
+            funcionEliminarCliente.execute();
+
+            coneLocal.commit();// confirmar si se ejecuto sin errores
+            respuesta = funcionEliminarCliente.getInt(1);// obtener salida
+        }
+        catch (SQLException e)
+        {
+            coneLocal.rollback();// deshacer la ejecucion en caso de error
+            System.out.println("Error al ejecutar función  por, " + e); // informar por consola
+        } finally {
+            coneLocal.close();// cerrar la conexion
+        }
+        return respuesta;
+    }
+
+    /**
+     * Retorna determinado valor, según respuesta de la función para eliminar a un Cliente por medio de su Id¿?
+     *
+     * @param IdCliente
+     * @return
+     * 1 = exitoso
+     * 2 = error al procesar datos
+     * @throws java.sql.SQLException
+     */
+    public int eliminarClientePorId( String IdCliente) throws SQLException {
+        int respuesta = 0;
+        Connection coneLocal = getNewConnection();
+
+        try {
+            coneLocal.setAutoCommit(false);
+            CallableStatement funcionEliminarCliente = coneLocal.prepareCall("{ ?=call xxxxx(?) }");
+
+            // cargar parametros de entrada
+            funcionEliminarCliente.setString(2, IdCliente);
+            funcionEliminarCliente.registerOutParameter(1, Types.INTEGER);//Parametro de salida
+            funcionEliminarCliente.execute();
+
+            coneLocal.commit();// confirmar si se ejecuto sin errores
+            respuesta = funcionEliminarCliente.getInt(1);// obtener salida
+        }
+        catch (SQLException e)
+        {
+            coneLocal.rollback();// deshacer la ejecucion en caso de error
+            System.out.println("Error al ejecutar función  por, " + e); // informar por consola
+        } finally {
+            coneLocal.close();// cerrar la conexion
+        }
+        return respuesta;
     }
 }
